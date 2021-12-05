39040faf08007093355b9709f38633174cc1ce75
Hoffmann <fredrik@hoffmanns.nu>
2021-12-05 15:24:02 +0100

    Made it even less readable and into less code

--- C-Sharp/AoC/Days/Five.cs
+++ C-Sharp/AoC/Days/Five.cs
@@ -5,22 +5,15 @@ namespace AoC.Days
 {
     internal class Five : Day
     {
-        public override string Part1() => execute(Helpers.GetStringInput($@"{GetType().Name}\{System.Reflection.MethodBase.GetCurrentMethod().Name}"), false);
-
-        public override string Part2() => execute(Helpers.GetStringInput($@"{GetType().Name}\{System.Reflection.MethodBase.GetCurrentMethod().Name}"), true);
-
-        private string execute(string[] rows, bool part2=false)
+        public override string Part1() => Execute(Helpers.GetStringInput($@"{GetType().Name}\{System.Reflection.MethodBase.GetCurrentMethod().Name}"));
+        public override string Part2() => Execute(Helpers.GetStringInput($@"{GetType().Name}\{System.Reflection.MethodBase.GetCurrentMethod().Name}"), true);
+        
+        private string Execute(string[] rows, bool part2=false)
         {
             var grid = new int[1000, 1000];
             foreach (string row in rows)
             {
-                string coordinatesLeft = GetLeft(row);
-                string coordinatesRight = GetRight(row);
-
-                int ly = int.Parse(coordinatesLeft.Split(',')[1]),
-                    ry = int.Parse(coordinatesRight.Split(',')[1]),
-                    lx = int.Parse(coordinatesLeft.Split(',')[0]),
-                    rx = int.Parse(coordinatesRight.Split(',')[0]);
+                var (lx, ly, rx, ry) = GetCoords(row);
 
                 if (ly == ry || lx == rx)
                 {
@@ -30,23 +23,25 @@ namespace AoC.Days
                         for (var j = 0; j < Math.Abs(yDiff) + 1; j++)
                             grid[lx + ((xDiff < 0) ? i : i * -1), ly + ((yDiff < 0) ? j : j * -1)]++;
                 }
-                else
+                else if((ly != ry || lx != rx) && part2)
                 {
-                    if (part2)
-                    {
-                        var diff = lx - rx;
-                        var xDir = lx - rx;
-                        var yDir = ly - ry;
-                        for (int xy = 0; xy < Math.Abs(diff) + 1; xy++)
-                            grid[lx + ((xDir < 0) ? xy : xy * -1), ly + ((yDir < 0) ? xy : xy * -1)]++;
-                    }
+                    var diff = lx - rx;
+                    var xDir = lx - rx;
+                    var yDir = ly - ry;
+                    for (int xy = 0; xy < Math.Abs(diff) + 1; xy++)
+                        grid[lx + ((xDir < 0) ? xy : xy * -1), ly + ((yDir < 0) ? xy : xy * -1)]++;
                 }
             }
 
             return grid.Cast<int>().Count(c => c >= 2).ToString();
         }
 
-        private string GetLeft(string formatted) => formatted.Substring(0, formatted.IndexOf('-') - 1);
-        private string GetRight(string formatted) => formatted.Substring(formatted.LastIndexOf('>') + 2, formatted.Length - formatted.LastIndexOf('>') - 2);
+        private (int lx, int ly, int rx, int ry) GetCoords(string formatted)
+        {
+            int[] left = GetLeft(formatted), right = GetRight(formatted);
+            return (left[0], left[1], right[0], right[1]);
+        }
+        private int[] GetLeft(string formatted) => Array.ConvertAll(formatted.Substring(0, formatted.IndexOf('-') - 1).Split(','), s => int.Parse(s));
+        private int[] GetRight(string formatted) => Array.ConvertAll(formatted.Substring(formatted.LastIndexOf('>') + 2, formatted.Length - formatted.LastIndexOf('>') - 2).Split(','), s => int.Parse(s));
     }
 }
